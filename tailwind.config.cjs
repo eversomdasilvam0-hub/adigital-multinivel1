1312d4587f7b95d5f7e93e44ab78fe0c9fec8619
[lasy] <git@lasy.ai>
2025-08-07 02:47:13 -0300

    [lasy] Corrigindo conflito de módulos nos arquivos de configuração - escreveu 3 arquivo(s) + extra files edited outside of Lasy

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,5 @@
-import type { Config } from "tailwindcss";
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{ts,tsx}",
@@ -93,4 +92,4 @@ export default {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config;
+};
--- vite.config.cjs
+++ vite.config.cjs
@@ -1,12 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { fileURLToPath } from "url";
+const { defineConfig } = require("vite");
+const react = require("@vitejs/plugin-react-swc");
+const path = require("path");
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-export default defineConfig({
+module.exports = defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
