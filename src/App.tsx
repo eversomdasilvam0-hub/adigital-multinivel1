b246e4f38885db4bd68475eace85de0f75eba0a5
[lasy] <git@lasy.ai>
2025-08-07 01:13:33 -0300

    [lasy] Corrigindo erros de deploy da Vercel - escreveu 2 arquivo(s), renamed 4 file(s) + extra files edited outside of Lasy

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Login from "./pages/Auth/Login";
-import Register from "./pages/Auth/Register";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
 import { AuthProvider } from "./contexts/AuthContext";
 import Layout from "./components/Layout";
 
